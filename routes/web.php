4d4cfefc6e1a7111c32d5117597a9acd756352ec
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-06 18:33:09 +0700

    chore(routes/web): added routing for trader login

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
-
+use App\Http\Controllers\UserAuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,3 +21,16 @@ Route::get('/', function () {
 // Route::get('/login', [App\Http\Controllers\AuthController::class, 'index'])->name('login');
 Route::get('/login', [AuthController::class, 'index'])->name('login');
 Route::post('/login', [AuthController::class, 'loginProcess']);
+
+// Route::get('/register', [AuthController::class, 'register']);
+// Route::post('/register', [AuthController::class, 'registerProcess']);
+
+Route::get('/logout', [AuthController::class, 'logout']);
+
+Route::get('/trader/login', [UserAuthController::class, 'index']);
+Route::post('/trader/login', [UserAuthController::class, 'loginProcess'])->name('trader.login.process');;
+
+
+// Route::get('/register/users', [UserAuthController::class, 'register']);
+// Route::post('/register/users', [UserAuthController::class, 'registerProcess']);
+// Route::get('/logout', [UserAuthController::class, 'logout']);
