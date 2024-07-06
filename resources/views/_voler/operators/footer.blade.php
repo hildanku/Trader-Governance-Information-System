8c2006a0d4d78ebf59e903085c5629ee6ae60cd0
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-06 19:06:30 +0700

    fix(_voler): fix the wrong asset url

--- resources/views/_voler/operators/footer.blade.php
+++ resources/views/_voler/operators/footer.blade.php
@@ -3,7 +3,7 @@
 <footer>
     <div class="footer clearfix mb-0 text-muted">
         <div class="float-start">
-            <p>2022 &copy; Template by Voler (Ahmad Saugi)</p>
+            <p>2022 &copy; Template by _voler (Ahmad Saugi)</p>
         </div>
         <div class="float-end">
             <p>Develop with <span class='text-danger'>😡 + &hearts; </span></i> by <a href="https://hildankutomo.my.id/">Kelompok 2</a></p>
@@ -13,14 +13,14 @@
 </footer>
 </div>
 </div>
-<script src="{{ asset('voler/dist/assets/js/feather-icons/feather.min.js') }}"></script>
-<script src="{{ asset('voler/dist/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
-<script src="{{ asset('voler/dist/assets/js/app.js') }}"></script>
+<script src="{{ asset('_voler/dist/assets/js/feather-icons/feather.min.js') }}"></script>
+<script src="{{ asset('_voler/dist/assets/vendors/perfect-scrollbar/perfect-scrollbar.min.js') }}"></script>
+<script src="{{ asset('_voler/dist/assets/js/app.js') }}"></script>
 
-<script src="{{ asset('voler/dist/assets/vendors/chartjs/Chart.min.js') }}"></script>
-<script src="{{ asset('voler/dist/assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
-<script src="{{ asset('voler/dist/assets/js/pages/dashboard.js') }}"></script>
+<script src="{{ asset('_voler/dist/assets/vendors/chartjs/Chart.min.js') }}"></script>
+<script src="{{ asset('_voler/dist/assets/vendors/apexcharts/apexcharts.min.js') }}"></script>
+<script src="{{ asset('_voler/dist/assets/js/pages/dashboard.js') }}"></script>
 
-<script src="{{ asset('voler/dist/assets/js/main.js') }}"></script>
+<script src="{{ asset('_voler/dist/assets/js/main.js') }}"></script>
 </body>
 </html>
--- resources/views/_voler/operators/header.blade.php
+++ resources/views/_voler/operators/header.blade.php
@@ -6,11 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard::Sistem Tata Kelola Pedagang</title>
     
-    <link rel="stylesheet" href="{{ asset('voler/dist/assets/css/bootstrap.css') }}">
+    <link rel="stylesheet" href="{{ asset('_voler/dist/assets/css/bootstrap.css') }}">
     
-    <link rel="stylesheet" href="{{ asset('voler/dist/assets/vendors/chartjs/Chart.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('_voler/dist/assets/vendors/chartjs/Chart.min.css') }}">
 
-    <link rel="stylesheet" href="{{ asset('voler/dist/assets/vendors/perfect-scrollbar/perfect-scrollbar.css') }}">
-    <link rel="stylesheet" href="{{ asset('voler/dist/assets/css/app.css') }}">
-    <link rel="shortcut icon" href="{{ asset('voler/dist/assets/images/favicon.svg') }}" type="image/x-icon">
-</head>
+    <link rel="stylesheet" href="{{ asset('_voler/dist/assets/vendors/perfect-scrollbar/perfect-scrollbar.css') }}">
+    <link rel="stylesheet" href="{{ asset('_voler/dist/assets/css/app.css') }}">
+    <link rel="shortcut icon" href="{{ asset('_voler/dist/assets/images/favicon.svg') }}" type="image/x-icon">
+</head> 
