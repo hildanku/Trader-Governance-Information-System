3d34a66572204773f4f9791312e6f6324ab125df
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-06 18:57:26 +0700

    chore: added sample views for operators and traders

---
+++ resources/views/operators/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('_voler.operators.master')
+
+@section('content')
+<h1> Operator Dashboard </h1>
+@endsection
---
+++ resources/views/traders/dashboard.blade.php
@@ -0,0 +1,5 @@
+@extends('_voler.operators.master')
+
+@section('content')
+<h1> Traders Dashboard </h1>
+@endsection
