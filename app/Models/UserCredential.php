6cc1917884a1b19a8d5c0540890ef566fb881765
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-06 23:25:33 +0700

    fix(UserCredential): fix conflict eloquent

--- app/Models/UserCredential.php
+++ app/Models/UserCredential.php
@@ -3,16 +3,13 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class UserCredential extends Authenticatable
 {
     use HasFactory;
 
-    protected $table = 'userCredentials';
+    protected $table = 'usercredentials';
     protected $fillable = ['fullname', 'username', 'email', 'password'];
-    protected $hidden = [
-        'password',
-    ];
+    protected $hidden = ['password'];
 }
