365d7329c10a54eefba19fc4a41f99accd3400f2
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-07 20:08:53 +0700

    feat: added views location management

---
+++ resources/views/operators/locationManagement/edit.blade.php
@@ -0,0 +1,101 @@
+@extends('_voler.operators.master')
+
+@section('content')
+
+<section id="basic-horizontal-layouts">
+    <div class="row match-height">
+        <div class="col-md-6 col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Add new location</h4>
+                </div>
+                <div class="card-content">
+                    <div class="card-body">
+                        <form class="form form-horizontal" method="POST" action="{{ route('operator.locations.update', $data->id) }}">
+                            @csrf
+                            <div class="form-body">
+                                <div class="row">
+                                    <div class="col-md-4">
+                                        <label>Location Code</label>
+                                    </div>
+                                    <div class="col-md-8 form-group">
+                                        <input type="text" class="form-control" name="locationCode" value="{{ $data->locationCode }}" required>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <label>Location Description</label>
+                                    </div>
+                                    <div class="col-md-8 form-group">
+                                        <input type="text" class="form-control" name="locationDescription" value="{{ $data->locationDescription }}" required>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <label>Location Latitude</label>
+                                    </div>
+                                    <div class="col-md-8 form-group">
+                                        <input type="text" class="form-control" id="locationLatitude" name="locationLatitude" value="{{ $data->locationLatitude }}" readonly>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <label>Location Longitude</label>
+                                    </div>
+                                    <div class="col-md-8 form-group">
+                                        <input type="text" class="form-control" id="locationLongitude" name="locationLongitude" value="{{ $data->locationLongitude }}" readonly>
+                                    </div>
+                                    <div class="col-sm-12 d-flex justify-content-end">
+                                        <button type="submit" class="btn btn-primary me-1 mb-1">Submit</button>
+                                        <button type="reset" class="btn btn-light-secondary me-1 mb-1">Reset</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6 col-12">
+            <div class="card">
+                <div class="card-header">
+                    <h4 class="card-title">Pick your location</h4>
+                </div>
+                <div class="card-content">
+                    <div class="card-body">
+                        <div id="map" style="height: 400px;"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<script src="https://unpkg.com/maplibre-gl@3.x/dist/maplibre-gl.js"></script>
+<link href="https://unpkg.com/maplibre-gl@3.x/dist/maplibre-gl.css" rel="stylesheet" />
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const apiKey = "v1.public.eyJqdGkiOiJmODVlNzExZC03MWE3LTQ3OTAtYWZjZC02ODBiOGM4OWMyMDYifZFF0YWJCtRWl0RrmK_4rEYz6RInVI3fmIXdAaleFg9o9pbDhc7A7VZ9VA-En-de2_zXaYIuup-b5a9EpTdbf-BjCSlN7yF8Bla8P2Gi-GqEQgokj9zLRDFJqYyDpqQM-xbL0ywbDL9i2pvB0uJeVmwKyce33xHDowN3GND2R9Ir1at4QRcuUMYBpljIUMTeH7pZXqgySRRQJYSK8gZ_gSZIPJGsAbTrXtZBaeeCTyaneCVCTnlGO914VbHrcH2td7LMYSl9MVggr-2Mawun6MRbrlsJ-p5ebCPXzoTaiEC963lHH4XRTx9-HENIFRIzIJrwCMHNrQMlwoDDX3Uec3M.ZWU0ZWIzMTktMWRhNi00Mzg0LTllMzYtNzlmMDU3MjRmYTkx";
+        const mapName = "sisfo_tatakelolapedagang";
+        const region = "us-east-1";
+        const lat = {{ $data->locationLatitude }};
+        const lng = {{ $data->locationLongitude }};
+
+        const map = new maplibregl.Map({
+            container: 'map',
+            style: `https://maps.geo.${region}.amazonaws.com/maps/v0/maps/${mapName}/style-descriptor?key=${apiKey}`,
+            center: [lng, lat],
+            zoom: 17
+        });
+
+        map.addControl(new maplibregl.NavigationControl(), 'top-left');
+
+        const marker = new maplibregl.Marker()
+            .setLngLat([lng, lat])
+            .addTo(map);
+
+        map.on('click', function(e) {
+            const lngLat = e.lngLat;
+            document.getElementById('locationLatitude').value = lngLat.lat;
+            document.getElementById('locationLongitude').value = lngLat.lng;
+
+            marker.setLngLat(lngLat);
+        });
+    });
+</script>
+@endsection
---
+++ resources/views/operators/locationManagement/index.blade.php
@@ -0,0 +1,98 @@
+@extends('_voler.operators.master')
+
+@section('content')
+<div class="row" id="basic-table">
+    <div class="col-12 col-md-12">
+      <div class="card">
+        <div class="card-header">
+          <h4 class="card-title">Location Management</h4>
+            <div class="float-end">
+              <a class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addModal">Add</a>
+            </div>
+        </div>
+        <div class="card-content">
+          {{-- <div class="d-flex float-end">
+            <a class="btn btn-success" data-bs-toggle="modal" data-bs-target="#addModal">Adsd</a>
+          </div> --}}
+          <div class="card-body">
+            {{-- <p class="card-text">Using the most basic table up, here’s how
+              <code>.table</code>-based tables look in Bootstrap. You can use any example
+              of below table for your table and it can be use with any type of bootstrap tables.</p> --}}
+            <!-- Table with outer spacing -->
+            {{-- <a href="{{ route('operators.userManagement.create') }}" class="btn btn-primary">Add User</a> --}}
+            <div class="table-responsive">
+              <table class="table">
+                <thead>
+                  <tr>
+                    <th>Location Code</th>
+                    <th>Location Description</th>
+                    <th>Latitude</th>
+                    <th>Longitude</th>
+                    <th>Area ID</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    @foreach ($datas as $data)
+                    <tr>
+                        <td>{{ $data->locationCode }}</td>
+                        <td>{{ $data->locationDescription }}</td>
+                        <td>{{ $data->locationLatitude }}</td>
+                        <td>{{ $data->locationLongitude }}</td>
+                        <td>{{ $data->areaId }}</td>
+                        <td>
+                            {{-- <a href="{{ route('operators.userManagement.show', $data->id) }}" class="btn btn-primary">View</a> --}}
+                            {{-- <a href="{{ route('operators.userManagement.edit', $data->id) }}" class="btn btn-primary">Edit</a>
+                            <a href="{{ route('operators.userManagement.destroy', $data->id) }}" class="btn btn-danger">Delete</a> --}}
+                            <button type="button" class="btn btn-danger btn-sm delete-btn" data-food-id="{{ $data->id }}" data-bs-toggle="modal" data-bs-target="#deleteModal{{ $data->id }}">
+                              Delete
+                            </button>
+                          </td>
+                    </tr>
+                  @endforeach
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+    <!-- Modal -->
+@foreach ($datas as $data)
+<div class="modal fade" id="deleteModal{{ $data->id }}" tabindex="-1" aria-labelledby="deleteModalLabel{{ $data->id }}" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="deleteModalLabel{{ $data->id }}">Delete Food</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <p>Are you sure you want to delete <strong>{{ $data->locationCode }}</strong>?</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <form id="deleteForm{{ $data->id }}" action="{{ Route('operator.locations.destroy', $data->id) }}" method="POST">
+          @csrf
+          @method('POST')
+          <button type="submit" class="btn btn-danger">Delete</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</div>
+@endforeach
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+<script>
+  $(document).ready(function() {
+      $('.delete-btn').click(function() {
+          var id = $(this).data('food-id');
+          var foodName = $(this).closest('tr').find('td:first').text().trim(); // Get food name from the first td of the current row
+          $('#deleteModalLabel' + id).text('Delete Food');
+          $('#deleteModal' + id).modal('show');
+      });
+    });
+</script>
+@endsection
