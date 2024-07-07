5cd5608cc43d61488d69057132b8f3ab8b9eb16d
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-07 20:07:33 +0700

    feat: add location management feature(crud)

---
+++ app/Http/Controllers/Operators/LocationManagementController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Operators;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+class LocationManagementController extends Controller
+{
+    public function index()
+    {
+        $datas = DB::table('locations')->get();
+        return view('operators.locationManagement.index', compact('datas'));
+    }
+
+    public function create()
+    {
+        return view('operators.locationManagement.create');
+    }
+
+    public function store(Request $request)
+    {
+        DB::table('locations')->insert([
+            'locationCode' => $request->locationCode,
+            'locationDescription' => $request->locationDescription,
+            'locationLatitude' => $request->locationLatitude,
+            'locationLongitude' => $request->locationLongitude,
+            'areaId' => 1,
+        ]);
+        return redirect('/operator/locations');
+    }
+
+    public function edit($locationId)
+    {
+        $data = DB::table('locations')->where('id', $locationId)->first();
+        return view('operators.locationManagement.edit', compact('data'));
+    }
+
+    public function update(Request $request, $locationId)
+    {
+        DB::table('locations')->where('id', $locationId)->update([
+            'locationCode' => $request->locationCode,
+            'locationDescription' => $request->locationDescription,
+            'locationLatitude' => $request->locationLatitude,
+            'locationLongitude' => $request->locationLongitude,
+        ]);
+        return redirect('/operator/locations');
+    }
+
+    public function destroy($locationId)
+    {
+        DB::table('locations')->where('id', $locationId)->delete();
+        return redirect('/operator/locations');
+    }
+}
