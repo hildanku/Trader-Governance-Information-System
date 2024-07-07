31cd84bf9b47ca7ea465177ac9a3527029250137
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-07 17:10:44 +0700

    feat(routes/web): add routing for areas management and init routing for users management

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserAuthController;
 use App\Http\Controllers\Operators\DashboardController as OperatorDashboardController;
 use App\Http\Controllers\Operators\UserManagementController;
+use App\Http\Controllers\Operators\AreaManagementController;
 use App\Http\Controllers\Traders\DashboardController as TraderDashboardController;
 use App\Http\Controllers\Traders\UserDetailController;
 
@@ -46,5 +47,29 @@ Route::middleware(['LoginCheck'])->group(function () {
     Route::get('/trader/profile', [UserDetailController::class, 'index'])->name('trader.profile');
 });
 
-Route::get('/operator/dashboard', [OperatorDashboardController::class, 'index'])->name('trader.dashboard');
+
 Route::get('/operator/users', [UserManagementController::class, 'index'])->name('operator.users');
+
+Route::middleware(['LoginCheck'])->group(function () {
+    // Dashboard
+    Route::get('/operator/dashboard', [OperatorDashboardController::class, 'index'])->name('trader.dashboard');
+    // End Dashboard
+
+    // Areas
+    Route::get('/operator/areas', [AreaManagementController::class, 'index'])->name('operator.areas');
+    Route::get('/operator/area/create', [AreaManagementController::class, 'create'])->name('operator.areas.create');
+    Route::post('/operator/area/store', [AreaManagementController::class, 'store'])->name('operator.areas.store');
+    Route::get('/operator/area/edit/{areaId}', [AreaManagementController::class, 'edit'])->name('operator.areas.edit');
+    Route::post('/operator/area/update/{areaId}', [AreaManagementController::class, 'update'])->name('operator.areas.update');
+    Route::post('operator/area/destroy/{areaId}', [AreaManagementController::class, 'destroy'])->name('operator.areas.destroy');
+    // End Areas
+
+    // Users
+    Route::get('/operator/users', [UserManagementController::class, 'index'])->name('operator.users');
+    Route::get('/operator/user/create', [UserManagementController::class, 'create'])->name('operator.users.create');
+    Route::post('/operator/user/store', [UserManagementController::class, 'store'])->name('operator.users.store');
+    Route::get('/operator/user/edit/{userId}', [UserManagementController::class, 'edit'])->name('operator.users.edit');
+    Route::post('/operator/user/update/{userId}', [UserManagementController::class, 'update'])->name('operator.users.update');
+    Route::post('operator/user/destroy/{userId}', [UserManagementController::class, 'destroy'])->name('operator.users.destroy');
+    // End Users
+});
