61e5847a953961bc0954fbf33e7d7783f9d33e16
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-09 06:12:52 +0700

    feat: add SubmissionManagementController

---
+++ app/Http/Controllers/Operators/SubmissionManagementController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers\Operators;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
+
+
+class SubmissionManagementController extends Controller
+{
+    public function index()
+    {
+        $datas = DB::table('submissions')->get();
+
+        return view('operators.submissionManagement.index', compact('datas'));
+    }
+    public function approveSubmission($submissionId)
+    {
+        $submissionData = DB::table('submissions')->where('id', $submissionId)->first();
+
+        $data = DB::table('submissions')
+        ->where('id', $submissionId)
+        ->update(['status' => "approved"]);
+        
+        $dataLocation = DB::table('locations')
+        ->where('id', $submissionData->locationId)
+        ->update(['isAvailable' => "no"]);
+
+        $insertPermit = DB::table('permits')
+        ->insert([
+            'submissionId' => $submissionData->id,
+            'permitNumber' => "TEST",
+            'issuedAt' => date('Y-m-d H:i:s'),
+            'expiredAt' => date('Y-m-d H:i:s', strtotime('+1 year')),
+        ]);
+
+        return redirect('/operator/submissions');
+        // return view('operators.submissionManagement.index', compact('datas'));
+    }
+
+    // public function approveSubmission($permit)
+    // {
+    //     $data = DB::table('permits')->where('id', $permit)->first();
+    //     return view('operators.userManagement.approveSubmission', compact('data'));
+    // }
+}
