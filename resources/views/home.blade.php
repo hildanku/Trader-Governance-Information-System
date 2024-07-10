edcad332e1db71939e893aae934889533a4d66dc
Hildan Kusto Utomo <hildankutomo@gmail.com>
2024-07-10 21:22:39 +0700

    chore(home): remove Operator Button login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
 
-<h1> Home </h1>
-<a href="{{ route('login') }}" class="btn btn-danger">Operator Login</a>
-<a href="{{ route('trader.login') }}" class="btn btn-danger">Trader Login</a>
+{{-- <h1> Home </h1> --}}
+<h2>Welcome! </h2>
+{{-- <a href="{{ route('login') }}" class="btn btn-danger">Operator Login</a> --}}
+<a href="{{ route('trader.login') }}" class="btn btn-danger">Login Here</a>
 @endsection
