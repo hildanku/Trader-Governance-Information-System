a2725016f6cc041720ae052d997054cab2afb24e
hildanku <hildankutomo@gmail.com>
2024-08-22 07:46:49 +0700

    fix(DashboardController): prevent trader users from accessing operator dashboard pages

--- app/Http/Controllers/Operators/DashboardController.php
+++ app/Http/Controllers/Operators/DashboardController.php
@@ -4,11 +4,15 @@ namespace App\Http\Controllers\Operators;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
-
+use Illuminate\Support\Facades\Auth;
 class DashboardController extends Controller
 {
     public function index()
     {
+        // ordinary user can't be here
+        if (!Auth::guard('web')->check()) {
+            return redirect('/operator/login');
+        }
         return view('operators.dashboard');
     }
 }
