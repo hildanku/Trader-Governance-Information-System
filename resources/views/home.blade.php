602291cfd00555ee221c67c0d1c9ec2fcd585d2e
hildanku <hildankutomo@gmail.com>
2024-08-23 14:28:08 +0700

    feat: added a session check feature when a user presses the website logo based on the role they are using

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,19 @@
 @extends('_voler.visitors.master')
 
 @section('content')
+@php
+    // Check if the user is authenticated
+    if (Auth::guard('web')->check()) {
+        // Redirect to operator dashboard if the user is an operator
+        header("Location: " . route('operator.dashboard'));
+        exit;
+    } elseif (Auth::guard('userCred')->check()) {
+        // Redirect to trader dashboard if the user is a trader
 
+        header("Location: " . route('trader.dashboard'));
+        exit;
+    }
+@endphp
 {{-- <h1> Home </h1> --}}
 <h2>Welcome! </h2>
 {{-- <a href="{{ route('login') }}" class="btn btn-danger">Operator Login</a> --}}
