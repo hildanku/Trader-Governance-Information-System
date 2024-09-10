ff0727227d3ffb3131b3789b95c23c8f444dfff6
hildanku <hildankutomo@gmail.com>
2024-09-10 07:45:35 +0700

    fix: handle null user name and display 'Visitor' as fallback

--- resources/views/_voler/layout/navbar.blade.php
+++ resources/views/_voler/layout/navbar.blade.php
@@ -135,7 +135,10 @@
                                 <div class="avatar me-1">
                                     <img src="{{ asset ('/_voler/dist/assets/images/avatar/avatar-s-1.png') }}" alt="" srcset="">
                                 </div>
-                                <div class="d-none d-md-block d-lg-inline-block">Hi, {{ Auth::guard('userCred')->user()->name }}!</div>
+                                {{-- logic --}}
+                                <div class="d-none d-md-block d-lg-inline-block">
+                                    Hi, {{ Auth::guard('userCred')->user()->name ?? 'Visitor' }}!
+                                </div>
                             </a>
                             <div class="dropdown-menu dropdown-menu-end">
                                 <div class="dropdown-divider"></div>
